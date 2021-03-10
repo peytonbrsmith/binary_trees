9019344c5f9800eadfb6303b76a911780d578247
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-10 15:55:52 -0600

    added function pointer check to both 7 and 8

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -12,6 +12,9 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL)
 		return;
 
+	if (func == NULL)
+		return;
+
 	binary_tree_inorder(tree->left, func);
 
 	(*func)(tree->n);
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -12,6 +12,9 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL)
 		return;
 
+	if (func == NULL)
+		return;
+
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
 	(*func)(tree->n);
