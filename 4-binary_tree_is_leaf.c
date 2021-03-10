a9a65de25454239852fbaf6c42460e466d95a266
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-10 15:41:25 -0600

    added new check for root

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -11,8 +11,16 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL)
 		return (0);
+
+	if (node->parent == NULL)
+	{
+		if (node->right == NULL || node->left == NULL)
+			return (1);
+		else
+			return (0);
+	}
 
 	if (node->right == NULL && node->left == NULL)
 	{
