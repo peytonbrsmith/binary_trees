e9a9afec1bf7c3d1b46f4c3c959bd6a7032ec9ed
peytonbrsmith <peytonbrsmith@gmail.com>
2021-03-10 15:49:52 -0600

    removed unneeded if clauses

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -14,21 +14,8 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (node == NULL)
 		return (0);
 
-	if (node->parent == NULL)
-	{
-		if (node->right == NULL || node->left == NULL)
-			return (1);
-		else
-			return (0);
-	}
-
 	if (node->right == NULL && node->left == NULL)
-	{
-		if (node->parent->right == NULL || node->parent->left == NULL)
-			return (1);
-		else
-			return (0);
-	}
+		return (1);
 
 	return (0);
 }
